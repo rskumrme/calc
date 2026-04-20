08f1cc30179012be6f585d01cc771fbed16d806d
rskumrme <rskumarme23@gmail.com>
2026-04-21 03:27:21 +0530

    test

---
+++ src/calc/Math.java
@@ -0,0 +1,32 @@
+package calc;
+
+public class Math {
+	
+	double var1, var2;
+
+	public double getVar1() {
+		return var1;
+	}
+
+	public void setVar1(double var1) {
+		this.var1 = var1;
+	}
+
+	public double getVar2() {
+		return var2;
+	}
+
+	public void setVar2(double var2) {
+		this.var2 = var2;
+	}
+	
+	public double add() {
+		return var1 + var2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
