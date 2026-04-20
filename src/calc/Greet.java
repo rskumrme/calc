37504c7429d48eafc81d72c2b0eaaf40c2597a11
rskumrme <rskumarme23@gmail.com>
2026-04-21 03:30:02 +0530

    test

--- src/calc/Greet.java
+++ src/calc/Greet.java
@@ -5,6 +5,12 @@ public class Greet {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		System.out.println("Siva");
+		
+		Math math = new Math();
+		math.setVar1(4);
+		math.setVar2(6);
+		
+		System.out.println(math.add());
 	}
 
 }
